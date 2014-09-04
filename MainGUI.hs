78d293dccb5f4cc138c03b8f0c1368b3796fc2f3
xuehy <xuehy@live.cn>
2014-09-04 15:46:18 +0800

    Alpha test

---
+++ MainGUI.hs
@@ -0,0 +1,308 @@
+module MainGUI where
+import Data.IORef
+import Graphics.UI.Gtk.Builder
+import Graphics.UI.Gtk
+import Control.Concurrent 
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit
+import Network.URI (parseURI)
+import System.Process
+import System.Posix.IO
+import System.IO
+import System.Directory
+import MusicAPI
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Text.Regex
+import Control.Monad.Error
+data GUI = GUI {
+	mainWin :: Window,
+	playButton :: Button,
+	pg :: ProgressBar,
+	time :: Label,
+	titleshow :: [Label],
+	authorshow :: [Label],
+	albumshow :: [Label],
+	inputEntry :: Entry,
+	searchButton :: Button,
+	mpage :: Label,
+	mprev :: Button,
+	mnext :: Button,
+	mselect :: [Button],
+	titlePlay :: Label
+}
+
+data LOGIN = LOGIN {
+	win :: Window,
+	info :: Label,
+	button :: Button,
+	username :: Entry,
+	passwd :: Entry
+}
+
+loadLogin :: FilePath -> IO LOGIN
+loadLogin gladepath = do
+	builder <- builderNew
+	builderAddFromFile builder gladepath
+	mw <- builderGetObject builder castToWindow "loginWin"
+	minfo <- builderGetObject builder castToLabel "loginInfo"
+	mb <- builderGetObject builder castToButton "loginButton"
+	mname <- builderGetObject builder castToEntry "usernameEntry"
+	mpass <- builderGetObject builder castToEntry "passwordEntry"
+	return $ LOGIN mw minfo mb mname mpass
+
+-- environment of player
+data ENV = ENV {
+	login :: IORef Bool,
+	passport :: IORef [String],
+	status :: IORef Bool,       -- playing or pause 
+	totalTime :: IORef Double,  -- current song's total length (in seconds)
+	curTime :: IORef Double,    -- current time position : range [0,1]
+	step :: IORef Double,       -- progress bar increase step
+	timePos :: IORef Int,       -- current time position : in seconds
+	totalSongs :: IORef Int,    -- total number of songs in the search result
+	pages :: IORef Int,         -- total pages of the result
+	currentPage :: IORef Int,   -- current page
+	songList :: IORef [Music],  -- song list of current page
+	currentSong :: IORef Int    -- id of current song playing in the list
+}
+
+initializeENV :: IO ENV
+initializeENV = do
+	lg <- newIORef False
+	pass <- newIORef []
+	st <- newIORef True
+	tt <- newIORef 0.0
+	ct <- newIORef 0.0
+	se <- newIORef 0.0
+	tp <- newIORef 0
+	total <- newIORef 0
+	page <- newIORef 0
+	curPage <- newIORef 0
+	song <- newIORef []
+	csong <- newIORef 0
+	return $ ENV lg pass st tt ct se tp total page curPage song csong
+
+searchAndShow :: GUI -> IORef ENV -> Int -> String -> IO ()
+searchAndShow gui env page keywords = do
+	s <- runMaybeT $ searchMusic page keywords
+	case s of 
+		Nothing -> return ()
+		Just (list,total) -> do
+			environment <- readIORef env 
+			modifyIORef (currentPage environment) (const page)
+			when (page == 1) 
+				$ do
+					modifyIORef (totalSongs environment) (const total)
+					let pageCnt = ceiling $ fromIntegral total / 10.0
+					modifyIORef (pages environment) (const pageCnt)
+			-- current list of result
+			modifyIORef (songList environment) (const list)
+			labelSetText (mpage gui) $ show page ++ " / " ++ (show . ceiling $ fromIntegral total / 10.0)
+			modifyIORef (currentPage environment) (const page)
+			zipWithM_ labelSetText (titleshow gui) (map (correctShow . 
+						(\x -> subRegex (mkRegex "<em>") x "") . 
+						(\x -> subRegex (mkRegex "</em>") x "") . title) list)
+			zipWithM_ labelSetText (authorshow gui) (map (correctShow . (\x -> subRegex (mkRegex "<em>") x "") . 
+						(\x -> subRegex (mkRegex "</em>") x "") . author) list)
+			zipWithM_ labelSetText (albumshow gui) (map (correctShow . (\x -> subRegex (mkRegex "<em>") x "") . 
+						(\x -> subRegex (mkRegex "</em>") x "") . albumTitle) list)
+
+
+loginFunc loginWin env = do
+	name <- entryGetText (username loginWin)
+	pass <- entryGetText (passwd loginWin)
+	eitherSession <- runErrorT $ getCookie name pass
+	case eitherSession of
+		Left Unreachable -> labelSetText (info loginWin) "Network unreachable"
+		Left LoginWrong -> labelSetText (info loginWin) "Wrong Username or password"
+		Left _ -> labelSetText (info loginWin) "Internal Error"
+		Right s -> do
+			environment <- readIORef env
+			modifyIORef (login environment) (const True)
+			modifyIORef (passport environment) (const s)
+			mainQuit
+
+waitForLogIn env = do
+	environment	<- readIORef env 
+	lg <- readIORef (login environment)
+	unless lg $ waitForLogIn env
+
+main :: FilePath -> IO()
+main gladepath = do
+	_ <- initGUI
+	_ <- timeoutAddFull (yield >> return True)
+				   priorityDefaultIdle 50	
+	env <- initializeENV >>= newIORef 
+	login <- loadLogin gladepath
+	button login `on` buttonActivated $ liftIO $ loginFunc login env
+	widgetShowAll $ win login
+	mainGUI
+	
+	waitForLogIn env
+
+	gui <- loadGlade gladepath
+	_ <- connectGui gui env
+	widgetShowAll $ mainWin gui
+	mainGUI
+
+loadGlade :: FilePath -> IO GUI
+loadGlade gladepath = do
+	builder <- builderNew
+	builderAddFromFile builder gladepath
+	mw <- builderGetObject builder castToWindow "window1"
+	mplay <- builderGetObject builder castToButton "button1"
+	mpg <- builderGetObject builder castToProgressBar "progressbar1"
+	mtime <- builderGetObject builder castToLabel "label1"
+	mtitle <- mapM (\x -> builderGetObject builder castToLabel $ 
+		           "label" ++ show x)
+	               [3..12]
+	mauthor <- mapM (\x -> builderGetObject builder castToLabel $ 
+		           "label" ++ show x)
+	               [13..22]
+	malbum <- mapM (\x -> builderGetObject builder castToLabel $ 
+		           "label" ++ show x)
+	               [23..32]
+	mentry <- builderGetObject builder castToEntry "searchFrame"
+	msearch <- builderGetObject builder castToButton "button3"
+	mpage <- builderGetObject builder castToLabel "label33"
+	prev <- builderGetObject builder castToButton "prev"
+	next <- builderGetObject builder castToButton "next"
+	play <- mapM (\x -> builderGetObject builder castToButton $ 
+		           "button" ++ show x)
+	               [4..13]
+	title <- builderGetObject builder castToLabel "label2"
+	return $ GUI mw mplay mpg mtime mtitle mauthor malbum mentry msearch mpage prev next play title
+
+clickPlay :: GUI -> IORef ENV -> Int -> IO ()
+clickPlay gui env songID = do
+	environment <- readIORef env
+	session <- readIORef (passport environment)
+	list <- readIORef $ songList environment
+	unless (songID >= length list) $ do
+		url <- makeDownloadURL session $ songId (list !! songID)
+
+		content <- simpleHttp $ "http://music.baidu.com" ++ url
+		unless (B.null content) $ do
+			forkIO (void (system "echo \"quit\" > /tmp/music\n")) >> threadDelay 1000000
+			B.writeFile "/tmp/tempmusic.mp3" content
+			doesFileExist "/tmp/music" >>= 
+				\x -> when x $ void (system "unlink /tmp/music") 
+			system "mkfifo /tmp/music"
+
+			threadDelay 2000000
+
+			(_,Just hout,_,_) <- createProcess (proc "mplayer" ["-slave","-quiet","-input","file=/tmp/music","/tmp/tempmusic.mp3"]) 
+												{std_out = CreatePipe}
+			threadDelay 100000
+			print "OK"
+			environment <- readIORef env 
+			system "echo \"get_time_length\" > /tmp/music" 
+			y <- forM [1..17] (\x -> hGetLine hout)
+			x <- hGetLine hout
+		 	modifyIORef (totalTime environment) (const . read $ drop 11 x)
+			total <- readIORef $ totalTime environment
+			modifyIORef (step environment) (const (1.0/total))
+			time <- newIORef 0.0
+			z <- forkIO $ void $ updateProgress gui env
+			z' <- forkIO $ void $ updateTime gui env
+			labelSetText (titlePlay gui) (correctShow . 
+							(\x -> subRegex (mkRegex "<em>") x "") . 
+							(\x -> subRegex (mkRegex "</em>") x "") . title $ list !! songID  )
+			return ()
+
+-- when the search button is clicked
+clickSearch :: GUI -> IORef ENV -> IO ()
+clickSearch gui env = do
+	keywords <- entryGetText (inputEntry gui)
+	_ <- timeoutAddFull (yield >> return True)
+				   priorityDefaultIdle 100
+	unless (null keywords) $ searchAndShow gui env 1 keywords
+
+prevPage :: GUI -> IORef ENV -> IO ()
+prevPage gui env = do
+	environment <- readIORef env
+	curPage <- readIORef (currentPage environment)
+	_ <- timeoutAddFull (yield >> return True)
+				   priorityDefaultIdle 100
+	unless (curPage == 1) $ do
+    	 keywords <- entryGetText (inputEntry gui)
+    	 modifyIORef (currentPage environment) (\x->x-1)
+    	 unless (null keywords) $ searchAndShow gui env (curPage-1) keywords
+
+nextPage :: GUI -> IORef ENV -> IO ()
+nextPage gui env = do 
+	environment <- readIORef env
+	totalPage <- readIORef (pages environment)
+	curPage <- readIORef (currentPage environment)
+	_ <- timeoutAddFull (yield >> return True)
+				   priorityDefaultIdle 100
+	when (curPage < totalPage) $ do
+    	 keywords <- entryGetText (inputEntry gui)
+    	 modifyIORef (currentPage environment) (+1)
+    	 unless (null keywords) $ searchAndShow gui env (curPage+1) keywords
+
+
+connectGui gui env = do
+	_ <- onDestroy (mainWin gui) $ forkIO (void (system "echo \"quit\" > /tmp/music\n")) >> mainQuit
+	onClicked (playButton gui) (guiPlay gui env)
+	searchButton gui `on` buttonActivated $ liftIO $ clickSearch gui env
+	mprev gui `on` buttonActivated $ liftIO $ prevPage gui env 
+	mnext gui `on` buttonActivated $ liftIO $ nextPage gui env 
+	zipWithM_ (\b i -> b `on` buttonActivated $ liftIO $ clickPlay gui env i)
+	          (mselect gui) [0..9]
+	mapM_ (`labelSetText` "") $ titleshow gui 
+	mapM_ (`labelSetText` "") $ authorshow gui 
+	mapM_ (`labelSetText` "") $ albumshow gui 
+
+
+-- update the progress bar 
+updateProgress gui env = do
+	environment <- readIORef env 
+	s <- readIORef $ status environment
+	step <- readIORef $ step environment
+	if s then 
+		readIORef (curTime environment) >>= 
+	    \t -> when (t < 1.0) $ progressBarSetFraction (pg gui) t >>
+		threadDelay 1000000 >>
+		modifyIORef (curTime environment) (+step) >>
+		updateProgress gui env
+	else threadDelay 1000000 >> updateProgress gui env
+
+-- update the time label
+updateTime gui env = do
+	environment <- readIORef env 
+	s <- readIORef $ status environment
+	total <- readIORef $ totalTime environment
+	tp <- readIORef $ timePos environment
+	let curTimeShow = showTimeI tp
+	let totalShow = showTime total
+	if s then 
+		labelSetText (time gui) (curTimeShow ++ "/" ++ totalShow) >> 
+		threadDelay 1000000 >>
+		modifyIORef (timePos environment) (+1) >>
+		updateTime gui env
+	else threadDelay 1000000 >> updateTime gui env
+
+showTimeI :: Int -> String 
+showTimeI y = show minutes ++ ":" ++ show seconds
+	where
+		minutes = y `div` 60
+		seconds = y `mod` 60
+		
+showTime :: Double -> String 
+showTime t = show minutes ++ ":" ++ show seconds
+	where
+		y = floor t
+		minutes = y `div` 60
+		seconds = y `mod` 60
+
+guiPlay :: GUI -> IORef ENV -> IO ()
+guiPlay gui env = do
+	environment <- readIORef env
+	modifyIORef (status environment) not 
+	system "echo \"pause\" > /tmp/music"
+	return ()
+
+
---
+++ MusicAPI.hs
@@ -0,0 +1,189 @@
+-- ghc -O2 -threaded MusicAPI.hs -package bytestring-0.9.2.1 -package network-2.3.0.13
+
+{-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
+module MusicAPI where
+
+import Text.XML.HXT.Core
+import Network.HTTP
+import qualified Data.Text.Lazy.Encoding as E
+import qualified Data.Text.Lazy.IO as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.ByteString.Lazy.Char8 as B
+import Control.Monad.Trans.Maybe
+import Network.URI
+import Text.Regex.Posix
+import Control.Monad.Error
+data Music =
+	Music { 
+		title :: String,
+		songId :: String,
+		author :: String,
+		albumTitle :: String,
+		lrclink :: String	
+ 	}
+  deriving (Show, Eq)
+
+
+atResponse = getChildren >>> hasName "search_common_response" >>> getChildren
+
+atSongList = atResponse >>> hasName "song_list" >>> getChildren
+
+getErrorCode = atResponse >>> deep (isElem >>> hasName "error_code")
+
+getPages = atResponse >>> hasName "pages" >>> getChildren >>> hasName "total" >>> getChildren >>> getText
+
+getElement str = getChildren >>> hasName str >>> getChildren >>> getText
+
+atTag str = deep (isElem >>> hasName str)
+
+getMusicList = atSongList >>> deep (isElem >>> hasName "song") >>>
+    proc x -> do
+      title'   <- getElement "title"       -< x 
+      sid      <- getElement "song_id"     -< x
+      author'  <- getElement "author"      -< x
+      album    <- getElement "album_title" `orElse` arr (const "") -< x
+      lrclink' <- getElement "lrclink" `orElse` arr (const "")     -< x 
+      returnA  -< Music {title = title', 
+                         songId = sid,
+                         author = author',
+                         albumTitle = album,
+                         lrclink = lrclink'}
+
+parseXML = readString [ withValidate no
+                      , withRemoveWS yes
+                      ] 
+
+fetchMusicList :: String -> IO (Maybe ([Music],Int))
+fetchMusicList url = do
+  rsp <- simpleHTTP $ getRequest url
+  doc <- getResponseBody rsp
+  if null doc 
+    then return Nothing
+    else do
+      let xml = parseXML doc
+      err' <- runX (xml >>> getErrorCode)
+      if not . null $ err' 
+        then return Nothing
+        else do
+          result <- runX (xml >>> getMusicList) 
+          total <- runX (xml >>> getPages) 
+          return . Just $ (result, read . head $ total)
+
+makeSearchURL :: Int -> String -> String
+makeSearchURL page keywords = 
+  "http://tingapi.ting.baidu.com/v2/restserver/ting?method=baidu.ting.search.common&query="
+  ++
+  urlEncode keywords
+  ++
+  "&page_size=10&page_no=" ++ show page ++ "&format=xml"
+
+searchMusic :: Int -> String -> MaybeT IO ([Music],Int)
+searchMusic page keywords = MaybeT . fetchMusicList $ makeSearchURL page keywords
+
+correctShow = TL.unpack . E.decodeUtf8 . B.pack
+
+parseHTML = readString [ withValidate no
+                       , withParseHTML yes
+                       , withWarnings no
+                       ]
+
+fetchDownloadURL = atTag "html" >>> atTag "body" >>> atTag "div" >>>
+                   hasAttrValue "class" (=="download-info clearfix") >>>
+                   getChildren >>>
+                   hasAttrValue "class" (=="download-wrapper") >>> 
+                   getChildren >>>
+                   hasAttrValue "class" (=="operation clearfix ") >>>
+                   getChildren >>> 
+                   hasAttr "data-btndata" >>>
+                   hasAttrValue "id" (=="128") >>>
+                   getAttrValue "href"
+
+-- needs login we should use cookie
+makeDownloadURL :: [String] -> String -> IO String
+makeDownloadURL session songID = do
+  let url = "http://music.baidu.com/song/" ++ songID
+         ++ "/download?__o%2Fsong%2F" ++ songID
+  let header = mkHeader HdrCookie $ "BAIDUID=" ++ session !! 1 ++ ":"
+          ++"FG=1; Hm_lvt_d0ad46e4afeacf34cd12de4c9b553aa6=1407811580;" 
+          ++"u_lo=0; u_id=; u_t=; bdshare_firstime=1405256709630;"
+          ++"PRY=1; MCITY=-321%3A360%3A; tracesrc=-1%7C%7Cwww.baidu.com;" 
+          ++"BDUSS=" ++ head session ++ ";"  
+          ++"BDRCVFR[OyOV3fcLK2t]=9LXhgZOj4kYfjbkPHcsnHmYgv99Udqs;" 
+          ++"BDRCVFR[QXr7TCQhxHb]=ddONZc2bo5mfAF9pywdpAqVuNqsus;" 
+          ++"H_PS_PSSID=1448_8235_8488_8056_8504_8593_7825_8579_7476_7799_8482_8319_8107_8548_8436"
+  let Just uri = parseURI url
+  rsp <- simpleHTTP $ Request uri GET [header] []
+  doc <- getResponseBody rsp
+  --putStrLn . TL.unpack . E.decodeUtf8 . B.pack $ doc
+  if null doc 
+    then return []
+    else do
+      let html = parseHTML doc
+      liftM head $ runX (html >>> fetchDownloadURL) 
+
+
+--main = do
+--  (getCookie >>= (\x -> liftIO $ makeDownloadURL x $ show 10525914) 
+--  print s
+--  r <- runErrorT getCookie
+--  let Right a = r
+--  print a
+
+
+
+-- simulate baidu login
+
+data LoginError = Unreachable | LoginWrong | OtherError String
+instance Error LoginError where
+  noMsg = OtherError "An Error"
+  strMsg = OtherError 
+
+getCookie :: String -> String -> ErrorT LoginError IO [String]
+getCookie name pass = do 
+  -- get BAIDUID
+  Right rsp <- liftIO $ simpleHTTP $ getRequest "http://www.baidu.com"
+  let maybeCookie = lookupHeader HdrSetCookie $ rspHeaders rsp
+  case maybeCookie of 
+    Nothing -> throwError Unreachable
+    Just cookie -> do 
+      -- get token
+      let apiurl = "http://passport.baidu.com/v2/api/?getapi&class=login&tpl=mn&tangram=true"
+      let Just uri = parseURI apiurl
+      resp <- liftIO $ simpleHTTP $ Request uri GET [mkHeader HdrCookie cookie] ""
+      doc <- liftIO $ getResponseBody resp
+      let pattern = "bdPass.api.params.login_token='(.)*'"
+      let token = take 32 $ drop 31 $ doc =~ pattern :: String
+   
+      -- log in
+      let baiduMainLoginUrl = "http://passport.baidu.com/v2/api/?login"
+      let staticPage = "http://www.baidu.com/cache/user/html/jump.html"
+      
+      let content = "charset=" ++ urlEncode "utf-8" ++ "&token="
+                    ++ urlEncode token ++ "&isPhone="
+                    ++ urlEncode "false" ++ "&index="
+                    ++ urlEncode "0" ++ "&staticpage="
+                    ++ urlEncode staticPage ++ "&loginType="
+                    ++ urlEncode "1" ++ "&tpl="
+                    ++ urlEncode "mn" ++ "&callback="
+                    ++ urlEncode "parent.bdPass.api.login._postCallback"
+                    ++ "&username=" ++ urlEncode name
+                    ++ "&password=" ++ urlEncode pass
+                    ++ "&mem_pass=" ++ urlEncode "on"
+
+      let postHeader = [mkHeader HdrCookie cookie, 
+                        mkHeader HdrContentType "application/x-www-form-urlencoded",
+                        mkHeader HdrContentLength $ show $ length content]
+      let Just loginuri = parseURI baiduMainLoginUrl
+      resp <- liftIO $ simpleHTTP $ Request loginuri POST postHeader content
+      case resp of
+        Left _ -> throwError LoginWrong
+        Right r -> do
+          let cookies = map (\(Header name str) -> if name == HdrSetCookie then str else "") $ rspHeaders r
+          let bduss = takeWhile (/=';') $ drop 6 $
+                        concatMap (=~ "BDUSS=(.)*; ") cookies :: String
+          let bduid = take 32 $ drop 8 $ 
+                        cookie =~ "BAIDUID=([0-9]|[A-Z]|[a-z])*" :: String
+          if null cookies || null bduss || null bduid
+            then throwError LoginWrong
+            else return [bduss,bduid]
+    
